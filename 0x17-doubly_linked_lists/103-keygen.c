6b7f9cb4adafcc6eb9ad81673c4c9c4d0d698e63
S1M0BH <mohamedbchiri90@gmail.com.com>
2023-06-16 02:57:27 +0000

    g

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,84 +3,115 @@
 #include <time.h>
 
 /**
- * find -> Finds the biggest number
- * @a: array
- * @n: Array Size
- * Return: biggest number
+ * f_4 =>finds the biggest number.
+ * @usrn: username.
+ * @len: length of username.
+ * Return: the biggest number.
  */
-
-int find(int *a, int n)
+int f_4(char *usrn, int len)
 {
-	int i, big;
+	int ch;
+	int vch;
+	unsigned int rand_num;
 
-	big = a[0];
-	for (i = 1; i < n; i++)
+	ch = *usrn;
+	vch = 0;
+
+	while (vch < len)
 	{
-		if (a[i] > big)
-			big = a[i];
+		if (ch < usrn[vch])
+			ch = usrn[vch];
+		vch += 1;
 	}
-	return (big);
+
+	srand(ch ^ 14);
+	rand_num = rand();
+
+	return (rand_num & 63);
 }
 
 /**
- * mult_char -> Multiplies each char of username
- * @usr: Username
- * @len: Username length
- * Return: Multiplied username
+ * f_5 =>multiplies each char of username.
+ * @usrn: username.
+ * @len: length of username.
+ * Return: multiplied char.
  */
-
-int mult_char(char *usr, int len)
+int f_5(char *usrn, int len)
 {
-	int i, mult;
+	int ch;
+	int vch;
 
-	mult = 1;
-	for (i = 0; i < len; i++)
-		mult *= usr[i];
-	return (mult);
-}
+	ch = vch = 0;
 
-/**
- * gen_pass -> Generates a random char
- * @usr: Username
- * Return: Random char
- */
-
-int gen_pass(char *usr)
-{
-	int i, len, mult, big, seed, key;
-	int arr[4];
-
-	len = 0;
-	while (usr[len] != '\0')
-		len++;
-	srand(time(NULL));
-	seed = rand();
-	srand(seed);
-	for (i = 0; i < 4; i++)
-		arr[i] = rand();
-	mult = mult_char(usr, len);
-	big = find(arr, 4);
-	key = (mult ^ big);
-	return (key);
-}
-
-/**
- * main -> Entry point
- * @argc: Number of arguments
- * @argv: Arguments
- * Return: 0 on success, 1 on failure
- */
-
-int main(int argc, char *argv[])
-{
-	int key;
-
-	if (argc != 2)
+	while (vch < len)
 	{
-		printf("Usage: %s username\n", argv[0]);
-		return (1);
+		ch = ch + usrn[vch] * usrn[vch];
+		vch += 1;
 	}
-	key = gen_pass(argv[1]);
-	printf("%d\n", key);
+
+	return (((unsigned int)ch ^ 239) & 63);
+}
+
+/**
+ * f_6 =>generates a random char.
+ * @usrn: username.
+ * Return : a random char.
+ */
+int f_6(char *usrn)
+{
+	int ch;
+	int vch;
+
+	ch = vch = 0;
+
+	while (vch < *usrn)
+	{
+		ch = rand();
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 229) & 63);
+}
+
+/**
+ * main =>Entry point.
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	char keygen[7];
+	int len, ch, vch;
+	long alph[] = {
+		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+		0x723161513346655a, 0x6b756f494b646850 };
+	(void) argc;
+
+	for (len = 0; argv[1][len]; len++)
+		;
+	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
+	ch = vch = 0;
+	while (vch < len)
+	{
+		ch = ch + argv[1][vch];
+		vch = vch + 1;
+	}
+	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
+	ch = 1;
+	vch = 0;
+	while (vch < len)
+	{
+		ch = argv[1][vch] * ch;
+		vch = vch + 1;
+	}
+	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
+	keygen[3] = ((char *)alph)[f_4(argv[1], len)];
+	keygen[4] = ((char *)alph)[f_5(argv[1], len)];
+	keygen[5] = ((char *)alph)[f_6(argv[1])];
+	keygen[6] = '\0';
+	for (ch = 0; keygen[ch]; ch++)
+		printf("%c", keygen[ch]);
 	return (0);
 }
