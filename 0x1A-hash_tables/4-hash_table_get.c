89b9fd60203a219e815c4cb4564666a0bde060dd
S1M0BH <mohamedbchiri90@gmail.com>
2023-07-07 04:48:50 -0700

    0x1A

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	tp = ht->array[index];
-	while (tp != NULL)kk
+	while (tp != NULL)
 	{
 		if (strcmp(tp->key, key) == 0)
 			return (tp->value);
